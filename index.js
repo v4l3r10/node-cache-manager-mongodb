c553ffbc5e556bf87bad9d5be578b88f1c1c7e13
Valerio Cavagni <valerio.cavagni@gmail.com>
2015-09-07 18:42:56 +0200

    Add ttl managed by MongoDb

--- index.js
+++ index.js
@@ -68,6 +68,18 @@ function MongoStore(args) {
       db.createCollection(store.coll, function (err, collection) {
         store.collection = collection;
       });
+      // Create an index on the a field
+      db.ensureIndex('ensureIndexExample1', {
+        expiresAt : 1
+      }, {
+        unique : true,
+        background : true,
+        w : 1,
+        expireAfterSeconds : store.MongoOptions.ttl
+      }, function (err, indexName) {
+        if (err)
+            console.log("Error During Indexes creation");
+      });
     });
   }
 
